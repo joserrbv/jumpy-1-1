d09267024b6bd77503d8fbb25163cd473afa89d5
joserrbv <josequeiroz40@gmail.com>
2023-01-11 00:38:45 -0400

    jumpy 1-1

---
+++ main.js
@@ -0,0 +1,14 @@
+function validateNumber(event) {
+    var key = window.event ? event.keyCode : event.which;
+    if (event.keyCode === 8 || event.keyCode === 46) {
+        return true;
+    } else if ( key < 48 || key > 57 ) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+$(document).ready(function(){
+    $('[id^=edit]').keypress(validateNumber);
+});
